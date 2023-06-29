5f3c72fcb7f2b63456b23ec31e0ed8b0175083ee
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-06-29 17:28:49 +0700

    86. Creating Documents and Testing the Model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -34,7 +34,21 @@ const tourSchema = new mongoose.Schema({
     },
 });
 
-const tour = mongoose.model('Tour', tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Park Camper',
+    price: 997,
+});
+
+testTour
+    .save()
+    .then((doc) => {
+        console.log(doc);
+    })
+    .catch((err) => {
+        console.log('ERROR: ', err);
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
