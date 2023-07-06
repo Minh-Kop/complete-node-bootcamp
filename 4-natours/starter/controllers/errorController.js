a6ec39b1e9744dcea63acde9d1bc3e91fc4185ae
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-07-06 17:14:50 +0700

    132. Protecting Tour Routes - Part 2

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -17,6 +17,14 @@ const handleValidationErrorDB = (err) => {
     return new AppError(message, 400);
 };
 
+const handleJWTError = () => {
+    return new AppError('Invalid token! Please log in again.', 401);
+};
+
+const handleJWTExpiredError = () => {
+    return new AppError('Your token has expired! Please log in again.', 401);
+};
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -68,6 +76,14 @@ module.exports = (err, req, res, next) => {
 
         if (err.name === 'ValidationError') {
             error = handleValidationErrorDB(error);
+        }
+
+        if (err.name === 'JsonWebTokenError') {
+            error = handleJWTError();
+        }
+
+        if (err.name === 'TokenExpiredError') {
+            error = handleJWTExpiredError();
         }
 
         sendErrorProd(error, res);
