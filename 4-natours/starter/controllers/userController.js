e7180c51141f6961170362e4f21756e1ce0f3284
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-07-07 17:15:54 +0700

    139. Updating the Current User: Data

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,7 +1,17 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 // const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
-// const AppError = require('../utils/appError');
+
+const filteredObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((el) => {
+        if (allowedFields.includes(el)) {
+            newObj[el] = obj[el];
+        }
+    });
+    return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
@@ -13,6 +23,32 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
         data: {
             users, // ES6
         },
+    });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // 1) Create error if user PATCHes password data
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(
+            new AppError(
+                'This route is not for password updates. Please use /updateMyPassword!',
+                400
+            )
+        );
+    }
+
+    // 2) Filter out unwanted field names that are not allowed to be updated
+    const filteredBody = filteredObj(req.body, 'name', 'email');
+
+    // 3) Update user document
+    const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
+        status: 'success',
+        user: updateUser,
     });
 });
 
