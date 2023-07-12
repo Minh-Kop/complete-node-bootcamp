f19d10af1ab7121342348667119ddaa9656e0c9d
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-07-12 22:21:37 +0700

    161. Building Handler Factory Functions: Delete

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 // const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 const filteredObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -82,9 +83,4 @@ exports.updateUser = (req, res) => {
     });
 };
 
-exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
-};
+exports.deleteUser = factory.deleteOne(User);
