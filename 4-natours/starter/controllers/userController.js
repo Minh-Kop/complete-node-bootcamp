811dcf985e208187f3bcc1eebd33bcb4e96823c0
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-07-13 08:53:49 +0700

    163. Factory Functions: Reading

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,6 +1,5 @@
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
-// const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
@@ -13,19 +12,6 @@ const filteredObj = (obj, ...allowedFields) => {
     });
     return newObj;
 };
-
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-    const users = await User.find();
-
-    // SEND RESPONSE
-    res.status(200).json({
-        status: 'success',
-        results: users.length,
-        data: {
-            users, // ES6
-        },
-    });
-});
 
 exports.updateMe = catchAsync(async (req, res, next) => {
     // 1) Create error if user PATCHes password data
@@ -62,19 +48,16 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
-};
-
 exports.createUser = (req, res) => {
     res.status(500).json({
         status: 'error',
-        message: 'This route is not yet defined!',
+        message: 'This route is not defined! Please use /signup instead.',
     });
 };
+
+exports.getAllUsers = factory.getAll(User);
+
+exports.getUser = factory.getOne(User);
 
 // Do NOT update PASSWORD with this
 exports.updateUser = factory.updateOne(User);
