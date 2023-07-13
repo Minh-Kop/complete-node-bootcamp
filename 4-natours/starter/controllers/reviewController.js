9ad03c89ad3e3d7b4eba8a949f6af25fee2549cb
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-07-13 07:51:28 +0700

    162. Factory Functions: Update and Create

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -19,7 +19,7 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
     // Allow nested routes
     if (!req.body.tour) {
         req.body.tour = req.params.tourId;
@@ -27,12 +27,12 @@ exports.createReview = catchAsync(async (req, res, next) => {
     if (!req.body.user) {
         req.body.user = req.user.id;
     }
-    const newReview = await Review.create(req.body);
 
-    res.status(201).json({
-        status: 'success',
-        review: newReview,
-    });
-});
+    next();
+};
+
+exports.createReview = factory.createOne(Review);
+
+exports.updateReview = factory.updateOne(Review);
 
 exports.deleteReview = factory.deleteOne(Review);
