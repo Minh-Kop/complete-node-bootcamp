f9fa87945bcbd65e0f1d2e3ef739294f8786b558
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-07-21 09:14:20 +0700

    214. Creating New Bookings on Checkout Success

--- 4-natours/starter/controllers/bookingController.js
+++ 4-natours/starter/controllers/bookingController.js
@@ -1,8 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // 1) Get the currently booked tour
@@ -12,7 +12,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     const session = await stripe.checkout.sessions.create({
         mode: 'payment',
         payment_method_types: ['card'],
-        success_url: `${req.protocol}://${req.get('host')}/`, // user will be redirected to this url when payment is successful. home page
+        success_url: `${req.protocol}://${req.get('host')}/?tour=${
+            req.params.tourId
+        }&user=${req.user.id}&price=${tour.price}`, // user will be redirected to this url when payment is successful. home page
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`, // user will be redirected to this url when payment has an issue. tour page (previous page)
         customer_email: req.user.email,
         client_reference_id: req.params.tourID, // this field allows us to pass in some data about this session that we are currently creating.
@@ -40,4 +42,16 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         status: 'success',
         session,
     });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+    // This is only temporary because it's UNSECURE: everyone can make bookings without paying
+    const { tour, user, price } = req.query;
+
+    if (!tour && !user && !price) {
+        return next();
+    }
+    await Booking.create({ tour, user, price });
+
+    res.redirect(req.originalUrl.split('?')[0]);
 });
